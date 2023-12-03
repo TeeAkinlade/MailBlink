dd93826add99422899c3353df87d9c1dac6dba26
Chris Awoke <uchechristian27@gmail.com>
2023-12-03 18:01:02 +0100

    New update

--- email-marketing-app/app/page.js
+++ email-marketing-app/app/page.js
@@ -2,13 +2,13 @@
 
 import Image from "next/image";
 
-import About from "./Home/About";
-import Campaign from "./Home/Campaign";
-import Footer from "./Home/Footer";
-import Hero from "./Home/Hero";
-import Nav from "./Home/Nav";
-import Services from "./Home/Services";
-import Subscribe from "./Home/Subscribe";
+import About from "../components/About";
+import Campaign from "../components/Campaign";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Nav from "../components/Nav";
+import Services from "../components/Services";
+import Subscribe from "../components/Subscribe";
 
 export default function Home() {
   return (
