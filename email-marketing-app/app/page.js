0da3b4f055e191898acc1778db8a632650ba92bd
Chris Awoke <uchechristian27@gmail.com>
2023-12-03 17:10:13 +0100

    updated

--- email-marketing-app/app/page.js
+++ email-marketing-app/app/page.js
@@ -1,9 +1,25 @@
+"use client";
+
 import Image from "next/image";
+
+import About from "./Home-components/About";
+import Campaign from "./Home-components/Campaign";
+import Footer from "./Home-components/Footer";
+import Hero from "./Home-components/Hero";
+import Nav from "./Home-components/Nav";
+import Services from "./Home-components/Services";
+import Subscribe from "./Home-components/Subscribe";
 
 export default function Home() {
   return (
-    <main >
-      <p className="font-poppins font-bold">Home</p>
+    <main>
+      <Nav />
+      <Hero />
+      <About />
+      <Campaign />
+      <Subscribe />
+      <Services />
+      <Footer />
     </main>
   );
 }
