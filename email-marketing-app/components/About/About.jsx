d14094289dc1211ccbc1d2848ea8de547cb5fadb
elijah ndenwa <elijahndenwa19@gmail.com>
2023-12-07 13:33:21 +0100

    added about page and components

--- email-marketing-app/components/About/About.jsx
+++ email-marketing-app/components/About/About.jsx
@@ -1,41 +1,29 @@
 import { RiVerifiedBadgeFill } from "react-icons/ri";
 import Image from 'next/image'
 
-const About = () => {
+const AboutSectionOne = () => {
   return (
     <section className="">
         <div className="grid grid-cols-1 md:grid-cols-2">
             <div className="px-8 md:px-0 md:pl-24 relative">
               <div className="pt-10 md:pt-12 pb-6 md:pb-48">
-                  <p className="text-primaryBlack2 text-3xl sm:text-4xl md:text-5xl font-semibold font-Roboto leading-tight">About Us <span className="text-forestGreen">Email Marketing</span> Made Easy.</p>
+                  <p className="text-primaryBlack2 text-3xl sm:text-4xl md:text-5xl font-semibold font-Roboto leading-tight">About Us</p>
                   <div className="my-6 md:my-8">
                     <p className="text-[#565656] text-sm md:text-lg">Who are we? MailBlink is a team of talented individuals working together to make software people love.</p>
                     <p className="text-[#565656] text-sm md:text-lg">We develop multi-award winning enterprise-class software that helps IT teams audit IT environments.</p>
-                    <button className="bg-navyBlue text-white text-sm md:text-lg px-3 md:px-5 py-2 ml-5 border border-navyBlue hover:text-navyBlue hover:font-semibold hover:border hover:border-navyBlue hover:bg-white transition duration-150">Let’s Talk</button>
-                    <button className="bg-navyBlue text-white text-sm md:text-lg px-3 md:px-5 py-2 ml-5 border border-navyBlue hover:text-navyBlue hover:font-semibold hover:border hover:border-navyBlue hover:bg-white transition duration-150">Let’s Talk</button>
-                    {/* <ul className="my-6">
-                      <li className="flex items-center text-sm md:text-base"><RiVerifiedBadgeFill className="text-navyBlue" size={24} /> <span className="ml-4 text-[#565656]">Get Overview at a glance</span></li>
-                      <li className="flex items-center text-sm md:text-base my-2"><RiVerifiedBadgeFill className="text-navyBlue" size={24} /> <span className="ml-4 text-[#565656]">Deposit funds easily, security</span></li>
-                      <li className="flex items-center text-sm md:text-base"><RiVerifiedBadgeFill className="text-navyBlue" size={24} /> <span className="ml-4 text-[#565656]">Get Live Support</span></li>
-                    </ul> */}
                   </div>
                   <div className="flex mt-8 md:mt-4 pb-10">
-                    <input type="text" placeholder="Enter email" className="px-3 md:px-5 py-2 focus:border-0 outline-none shadow-xl cursor-pointer" />
-                    <button className="bg-navyBlue text-white text-sm md:text-lg px-3 md:px-5 py-2 ml-5 border border-navyBlue hover:text-navyBlue hover:font-semibold hover:border hover:border-navyBlue hover:bg-white transition duration-150">Let’s Talk</button>
+                    <button className="bg-navyBlue text-white text-sm md:text-lg px-3 md:px-5 py-2 ml-5 border border-navyBlue hover:text-navyBlue hover:font-semibold hover:border hover:border-navyBlue hover:bg-white transition duration-150">Contact Us</button>
+                    <button className="bg-navyBlue text-white text-sm md:text-lg px-3 md:px-5 py-2 ml-5 border border-navyBlue hover:text-navyBlue hover:font-semibold hover:border hover:border-navyBlue hover:bg-white transition duration-150">Book a Demo</button>
                   </div>
-                </div>
-                <div className="">
-                  <Image src='/image/Rectangle.svg' alt="left-half-circle" width="24" height="24" className=" absolute left-0 top-0" />
-                  <Image src='/image/Rectangle (1).svg' alt="left-half-cirle 2" width="64" height="44" className="hidden md:block absolute left-0 bottom-0" />
-                  <Image src='/image/Rectangle (2).svg' alt="left-half-cirle 2" width="20" height="20" className="absolute right-0 bottom-4 md:bottom-10" />
                 </div>
             </div>
             <div className="pr-4">
-              <Image src='/image/Group 1000000796.svg' alt="hero-dashboard-image" width='150' height='150' className="w-full object-cover" />
+              <Image src='/image/About-hero.svg' alt="About-hero" width='150' height='150' className="w-full object-cover" />
             </div>
         </div>
     </section>
   )
 }
 
-export default About;
+export default AboutSectionOne;
--- email-marketing-app/components/About/AboutSectionOne.jsx
+++ email-marketing-app/components/About/AboutSectionOne.jsx
@@ -1,107 +1,28 @@
-import Image from "next/image";
-
-
-
-
-
-const SectionTitle = ({
-  title,
-  paragraph,
-  width = "570px",
-  center,
-  mb = "100px",
-}) => {
-  return (
-    <>
-      <div
-        className={`wow fadeInUp w-full ${center ? "mx-auto text-center" : ""}`}
-        data-wow-delay=".1s"
-        style={{ maxWidth: width, marginBottom: mb }}
-      >
-        <h2 className="mb-4 text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[45px]">
-          {title}
-        </h2>
-        <p className="text-base !leading-relaxed text-body-color md:text-lg">
-          {paragraph}
-        </p>
-      </div>
-    </>
-  );
-};
-
-
-const checkIcon = (
-  <svg width="16" height="13" viewBox="0 0 16 13" className="fill-current">
-    <path d="M5.8535 12.6631C5.65824 12.8584 5.34166 12.8584 5.1464 12.6631L0.678505 8.1952C0.483242 7.99994 0.483242 7.68336 0.678505 7.4881L2.32921 5.83739C2.52467 5.64193 2.84166 5.64216 3.03684 5.83791L5.14622 7.95354C5.34147 8.14936 5.65859 8.14952 5.85403 7.95388L13.3797 0.420561C13.575 0.22513 13.8917 0.225051 14.087 0.420383L15.7381 2.07143C15.9333 2.26669 15.9333 2.58327 15.7381 2.77854L5.8535 12.6631Z" />
-  </svg>
-);
+import Image from 'next/image'
 
 const AboutSectionOne = () => {
-  const List = ({ text }) => (
-    <p className="mb-5 flex items-center text-lg font-medium text-body-color">
-      <span className="mr-4 flex h-[30px] w-[30px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary">
-        {checkIcon}
-      </span>
-      {text}
-    </p>
-  );
-
   return (
-    <section id="about" className="pt-16 md:pt-20 lg:pt-28">
-      <div className="container">
-        <div className="border-b border-body-color/[.15] pb-16 dark:border-white/[.15] md:pb-20 lg:pb-28">
-          <div className="-mx-4 flex flex-wrap items-center">
-            <div className="w-full px-4 lg:w-1/2">
-              <SectionTitle
-                title="Our Mission"
-                paragraph="Empower youths and young adults, instilling hope, while spreading the eternal message of Christ as outlined in Revelation 14:6-12, aligning with the Great Commission in Matthew 28:18-20 to make disciples of all nations."
-                mb="44px"
-              />
-
-              <div
-                className="wow fadeInUp mb-12 max-w-[570px] lg:mb-0"
-                data-wow-delay=".15s"
-              >
-                <div className="mx-[-12px] flex flex-wrap">
-                  <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="Implement transformative programs for youths to positively impact society." />
-                    <List text="Empower young adults, especially in developing countries." />
-                    <List text="Create small groups in educational institutions for relationship-building and integration." />
+    <section className="">
+        <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="px-8 md:px-0 md:pl-24 relative">
+              <div className="pt-10 md:pt-12 pb-6 md:pb-48">
+                  <p className="text-primaryBlack2 text-3xl sm:text-4xl md:text-5xl font-semibold font-Roboto leading-tight">About Us</p>
+                  <div className="my-6 md:my-8">
+                    <p className="text-[#565656] text-sm md:text-lg">Who are we? MailBlink is a team of talented individuals working together to make software people love.</p>
+                    <p className="text-[#565656] text-sm md:text-lg">We develop multi-award winning enterprise-class software that helps IT teams audit IT environments.</p>
                   </div>
-
-                  <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="Establish a regular support center for academic and life issues in higher institutions." />
-                    <List text="Evaluate and address the needs of youths in developing countries, starting with Nigeria." />
-                    <List text="Organize vocational studies to equip young adults for self-employment and societal impact." />
+                  <div className="flex mt-8 md:mt-4 pb-10">
+                    <button className="bg-navyBlue text-white text-sm md:text-lg px-3 md:px-5 py-2 ml-5 border border-navyBlue hover:text-navyBlue hover:font-semibold hover:border hover:border-navyBlue hover:bg-white transition duration-150">Contact Us</button>
+                    <button className="bg-navyBlue text-white text-sm md:text-lg px-3 md:px-5 py-2 ml-5 border border-navyBlue hover:text-navyBlue hover:font-semibold hover:border hover:border-navyBlue hover:bg-white transition duration-150">Book a Demo</button>
                   </div>
                 </div>
-              </div>
             </div>
-
-            <div className="w-full px-4 lg:w-1/2">
-              <div
-                className="wow fadeInUp relative mx-auto aspect-[25/24] max-w-[500px] lg:mr-0"
-                data-wow-delay=".2s"
-              >
-                <Image
-                  src="/assets/images/About-hero.svg"
-                  alt="about-image"
-                  fill
-                  className="mx-auto max-w-full drop-shadow-three dark:hidden dark:drop-shadow-none lg:mr-0"
-                />
-                <Image
-                  src="/assets/images/About-hero.svg"
-                  alt="about-image"
-                  fill
-                  className="mx-auto hidden max-w-full drop-shadow-three dark:block dark:drop-shadow-none lg:mr-0"
-                />
-              </div>
+            <div className="pr-4">
+              <Image src='/image/About-hero.svg' alt="About-hero" width='150' height='150' className="w-full object-cover" />
             </div>
-          </div>
         </div>
-      </div>
     </section>
-  );
-};
+  )
+}
 
 export default AboutSectionOne;
