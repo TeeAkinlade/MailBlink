26bf12a5d1ca4148e1b3ac6bf63296b6f022da25
Chris Awoke <uchechristian27@gmail.com>
2023-12-18 21:24:32 +0100

    Update page.jsx

--- email-marketing-app/app/about/page.jsx
+++ email-marketing-app/app/about/page.jsx
@@ -1,15 +1,5 @@
-import React from "react";
-import Form from "../../components/Form";
-import AboutSectionOne from "../../components/About/AboutSectionTwo";
-import AboutSectionTwo from "../../components/About/AboutSectionTwo";
-
 const About = () => {
-  return <div>
-    <AboutSectionOne/>
-    <AboutSectionTwo/>
-    
-    <Form/>
-  </div>;
+  return <div>About</div>;
 };
 
 export default About;
