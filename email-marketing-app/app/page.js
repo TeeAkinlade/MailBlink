8cdc1f886b30a94841aec4e72d4a620f93e4f45b
elijah ndenwa <elijahndenwa19@gmail.com>
2023-12-20 08:40:52 +0100

    about page added

--- email-marketing-app/app/page.js
+++ email-marketing-app/app/page.js
@@ -3,16 +3,16 @@ import Campaign from "./home/Campaign";
 import Subscribe from "./home/Subscribe";
 import Services from "./home/Services";
 import Footer from "./home/footer/Footer";
-import About from "./home/About";
 import Nav from "./home/nav/Nav";
 import Hero from "./home/Hero";
+import About from "./about/about";
 
 export default function Home() {
   return (
     <main>
       <Nav/>
       <Hero />
-      <About />
+      <About/>
       <Campaign />
       <Subscribe />
       <Services />
